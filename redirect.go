529b2355e97fadb56241a941e086f4a5b43a6d24
Filip Van Raemdonck <37330756+fvanraem@users.noreply.github.com>
2024-07-01 17:55:50 +0200

    Add minimal implementation

---
+++ redirect.go
@@ -0,0 +1,44 @@
+// vim: set noet ts=4 sw=4 tw=79 cc=80 list listchars=tab\:\ \ ,lead\:·,trail\:·:
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+var (
+	help        bool
+	redirectURL string
+)
+
+func init() {
+	flag.StringVar(&redirectURL, "redirect-url", "", "URL to redirect to (required)")
+	flag.BoolVar(&help, "help", false, "Show help message")
+
+	flag.Parse()
+
+	if help {
+		fmt.Println("Usage: go run main.go [--redirect-url=<url>] ")
+		fmt.Println("  --redirect-url=<url>: URL to redirect all requests to.")
+		fmt.Println("  -h, --help: Display this help message.")
+		os.Exit(0)
+	}
+
+	if redirectURL == "" {
+		fmt.Println("Error: --redirect-url flag is required")
+		flag.Usage()
+		os.Exit(1)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", RedirectServer)
+	http.ListenAndServe(":8080", nil)
+}
+
+func RedirectServer(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, redirectURL, http.StatusMovedPermanently)
+}
